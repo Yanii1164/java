82db46397d7e9e5abe33763d6cb263960d835c2e
山根 祥 <sho.yamane@recochoku.co.jp>
2018-10-01 18:30:32 +0900

    テスト

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+* サンプルコード Main
+* 開発者：山根
+* 作成日：2018/10/01
+*/
+
+public class Main {
+    // ここからMainメソッド
+    public static void main(String[] args) {
+        System.out.println("やまね");
+        System.out.println("25歳");
+        System.out.println("お酒が苦手です");
+        System.out.println("計算↓");
+        System.out.println(31 + 31);
+        int x;
+        x = 6;
+        System.out.println(x * x * 3.14);
+    }
+}
---
+++ src/pra0103.java
@@ -0,0 +1,15 @@
+/*
+ * 練習問題1-3コード pra0103
+ * 開発者：山根
+ * 作成日：2018/10/01
+ */
+
+public class pra0103 {
+    public static void main(String[] args) {
+        boolean a = true;
+        char b = '駆';
+        double c = 3.14;
+        double d = 314159265853979L;
+        String e = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    }
+}
